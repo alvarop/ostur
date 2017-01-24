2f767bba32643d5f6fd64e6901c74ba786916246
alvaro <gitkraken@alvaroprieto.com>
2017-01-24 06:13:51 -0800

    Update Ostur.py to work with ostur controller fw (not silta)

--- ostur_controller/Ostur.py
+++ ostur_controller/Ostur.py
@@ -4,67 +4,109 @@
  https://github.com/adafruit/Adafruit_AM2315
 '''
 
-from silta import stm32f407
+import serial
 from am2315 import AM2315
 from mcp970x import MCP9701
 import sys
 import time
 
-relays = {
-    'fridge': {'pin':'PD0'},
-    'humidifier': {'pin':'PD1'},
-    'water_pump': {'pin':'PD2'}
-}
 
 class InvalidRelayException(Exception):
     pass
 
 class Ostur:
-    def __init__(self, bridge_file):
-        self.bridge = stm32f407.bridge(bridge_file)
 
-        self.relays = relays
+    DEBUG = False
 
-        self.thsensor = AM2315(self.bridge)
+    # Hardcoding until we can read values back from device
+    __CMD_MAX_STR_LEN = 1024
+    __SPI_MAX_BYTES = 512
+    __I2C_MAX_BYTES = 512
 
-        self.t1 = MCP9701(self.bridge, 'PA0')
-        self.t2 = MCP9701(self.bridge, 'PA1')
+    def __init__(self, serial_device, baud_rate=None):
+        self.stream = None
 
-        self.bridge.gpiocfg(self.relays['fridge']['pin'], 'output')
-        self.bridge.gpiocfg(self.relays['humidifier']['pin'], 'output')
-        self.bridge.gpiocfg(self.relays['water_pump']['pin'], 'output')
+        try:
+            self.stream = serial.Serial()
+            self.stream.port = serial_device
+            self.stream.timeout = 0.1
+            if baud_rate:
+                self.stream.baudrate = baud_rate
+            self.stream.open()
+        except OSError:
+            raise IOError('could not open ' + serial_device)
+
+        if self.stream:
+            self.stream.flush()
+
+        # Flush any remaining data in the silta's buffer
+        self.__send_cmd('\n')
+
+        # Get device serial number and save it
+        line = self.__send_cmd('sn\n')
+        result = line.strip().split(' ')
+
+        if result[0] == 'OK':
+            self.serial_number = ''.join(result[1:])
+        else:
+            self.serial_number = None
+            print('Warning: Could not read device serial number.')
+            print('You might want to update firmware on your board')
+
+        # Get device serial number and save it
+        line = self.__send_cmd('version\n')
+        result = line.strip().split(' ')
+
+        if result[0] == 'OK':
+            self.firmware_version = result[1]
+        else:
+            self.firmware_version = None
+            print('Warning: Could not read device firmware version.')
+            print('You might want to update firmware on your board')
+
+    def close(self):
+        ''' Disconnect from USB-serial device. '''
+        self.stream.close()
+
+    # Send terminal command and wait for response
+    def __send_cmd(self, cmd):
+
+        if (len(cmd) + 1) > self.__CMD_MAX_STR_LEN:
+            raise RuntimeException('Command string too long')
+
+        self.stream.write(cmd + '\n')
+        if self.DEBUG is True:
+            print 'CMD : ' + cmd
+
+        line = self.stream.readline()
+        if self.DEBUG is True:
+            print 'RESP: ' + line,
+
+        return line
+
+    def start_sampling(self):
+        self.stream.flush()
+        line = self.__send_cmd('controller start')
+        self.stream.timeout = 5  # This should be dependent on sample rate
+
+        result = line.strip().split(' ')
+        if result[0] == 'DATA':
+            # Return header without DATA and the last comma
+            return (result[1].strip()[:-1]).split(',')
+        else:
+            return result
+
+    def stop_sampling(self):
+        self.__send_cmd('controller stop')
+        self.stream.timeout = 0.1
 
     def read_sensors(self):
         sensors = {}
+        line = self.stream.readline()
 
-        humidity0, temp0 = self.thsensor.read()
-        temp1 = self.t1.read()
-        temp2 = self.t2.read()
-
-        sensors['time'] = time.time()
-        sensors['humidity'] = humidity0
-        sensors['temp0'] = temp0
-        sensors['temp1'] = temp1
-        sensors['temp2'] = temp2
+        result = line.strip().split(' ')
+        if result[0] == 'DATA':
+            # Return samples without DATA and the last comma
+            return (result[1].strip()[:-1]).split(',')
 
         return sensors
-
-    def relay_control(self, relay, state=False):
-        if relay not in self.relays:
-            raise InvalidRelayException('Invalid Relay ' + relay)
-
-        if state is True:
-            self.bridge.gpio(self.relays[relay]['pin'], 1)
-        else:
-            self.bridge.gpio(self.relays[relay]['pin'], 0)
-
-    def relay_list(self):
-        return sorted(self.relays.keys())
-
-    def relay_status(self):
-        status = {}
-        for key in sorted(self.relays.keys()):
-            status[key] = self.bridge.gpio(self.relays[key]['pin'])
-
-        return status
-
