11b09b43e30dbdf25348f65add0046d79dead892
alvaro <gitkraken@alvaroprieto.com>
2017-01-26 22:24:53 -0800

    Updating sweet db merge tool

--- ostur_controller/thmerge.py
+++ ostur_controller/thmerge.py
@@ -6,10 +6,14 @@ import time
 import sqlite3
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-def read(db_name, con, cur):
+def timestampFromFilename(filename):
+    dt = datetime.strptime(filename,'th_log_%Y-%m-%d_%H-%M-%S.db')
+    return int(time.mktime(dt.timetuple()))
 
-    con_r = None
+def read(db_name, con, cur, actual_start_time = 0):
+
     con_r = sqlite3.connect(db_name)
 
     if con_r is None:
@@ -20,24 +24,20 @@ def read(db_name, con, cur):
 
     rows = cur_r.fetchall()
 
-    for row in rows:
-        sql_insert = "INSERT INTO samples VALUES(NULL,{})".format(','.join(['?']*(len(row)-1)))
+    first_timestamp = rows[0][1]
 
+    for row in rows:
+        row = list(row)
+        row[1] = row[1] - first_timestamp + actual_start_time * 1000
+        sql_insert = "INSERT INTO samples VALUES(NULL,{})".format(','.join(['?']*(len(row)-1)))
         cur.execute(sql_insert, row[1:])
 
     con.commit()
 
-th_sensors = [
-    ('sht31_0', None),
-    ('sht31_1', None),
-    ('sht31_2', None),
-    ('sht31_5', None),
-    ('sht31_6', None),
-    ('sht31_7', None),
-]
+NUM_SENSORS = 3
 
 # Create new sqlite db for every run (maybe later do day-by-day?)
-db_name = time.strftime('th_log_%Y-%m-%d_%H-%M-%S.db', time.localtime())
+db_name = time.strftime('merged.db', time.localtime())
 
 con = None
 con = sqlite3.connect(db_name)
@@ -46,14 +46,17 @@ if con is None:
     raise IOError('Unable to open sqlite database')
 
 names = ['timestamp']
-for name,sensor in th_sensors:
-    names.append('{}_humidity'.format(name))
-    names.append('{}_temperature'.format(name))
+for sensor in range(NUM_SENSORS):
+    names.append('sensor{}_t'.format(sensor))
+    names.append('sensor{}_h'.format(sensor))
+
+print names
 
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS samples(id INTEGER PRIMARY KEY,{} INTEGER)".format(' INTEGER, '.join(names)))
 
-
-read(sys.argv[1], con, cur)
-read(sys.argv[2], con, cur)
-
+for file in range(1,len(sys.argv)):
+    filename = sys.argv[file]
+    timestamp = timestampFromFilename(filename)
+    print("Reading file %s starting at %d" % (filename,timestamp))
+    read(filename, con, cur, timestamp)
