186310dfb0db2f7e79a8a9580068cedc258c1a76
alvaro <gitkraken@alvaroprieto.com>
2018-02-04 13:54:59 -0800

    Adding bglib

---
+++ sw/2.0/lib/bglib/setup.py
@@ -0,0 +1,14 @@
+"""
+BGLib wrapper
+"""
+from setuptools import setup
+
+setup(name='bglib',
+      version='0.1',
+      description='BGLib for BLED112',
+      url='https://github.com/jrowberg/bglib',
+      author='Jeff Rowberg',
+      author_email='jeff@rowberg.net',
+      license='MIT',
+      packages=['bglib'],
+      zip_safe=False)
