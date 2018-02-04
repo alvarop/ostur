576e836c286217c2d74610fe6f21b695e5f12c9e
alvaro <gitkraken@alvaroprieto.com>
2018-02-04 08:16:49 -0800

    Got basic BLE working!

--- fw/2.0/apps/blinky/src/main.c
+++ fw/2.0/apps/blinky/src/main.c
@@ -8,27 +8,91 @@
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
 
+#if MYNEWT_VAL(USE_BLE)
+#include <host/ble_hs.h>
+#endif
+
 #define OSTUR_TASK_PRI         (10)
 #define OSTUR_STACK_SIZE       (256)
 struct os_task ostur_task;
 os_stack_t ostur_task_stack[OSTUR_STACK_SIZE];
 
-void ostur_task_fn(void *arg) {
-    uint8_t count = 0;
+static uint16_t timestamp;
 
+#if MYNEWT_VAL(USE_BLE)
+#define BEACON_MAGIC 0xDA7A
+
+typedef struct {
+    uint16_t magic;
+    uint16_t timestamp;
+    uint8_t dummy[12];
+} __attribute__((packed)) ble_beacon_t;
+
+static ble_beacon_t beacon_data;
+
+static void ble_app_advertise();
+
+static void ble_app_set_addr(void) {
+    ble_addr_t addr;
+    int rc;
+
+    rc = ble_hs_id_gen_rnd(1, &addr);
+    assert(rc == 0);
+
+    rc = ble_hs_id_set_rnd(addr.val);
+    assert(rc == 0);
+}
+
+static void ble_app_advertise() {
+    struct ble_gap_adv_params adv_params;
+    struct ble_hs_adv_fields fields;
+    int rc;
+
+    beacon_data.timestamp = timestamp;
+
+    fields = (struct ble_hs_adv_fields){ 0 };
+    rc = ble_eddystone_set_adv_data_uid(&fields, &beacon_data);
+    assert(rc == 0);
+
+    adv_params = (struct ble_gap_adv_params){ 0 };
+    rc = ble_gap_adv_start(
+        BLE_OWN_ADDR_RANDOM,
+        NULL,
+        500,
+        &adv_params,
+        NULL,
+        NULL);
+    assert(rc == 0);
+}
+
+static void ble_app_on_sync(void) {
+    /* Generate a non-resolvable private address. */
+    ble_app_set_addr();
+}
+#endif
+
+void ostur_task_fn(void *arg) {
     hal_gpio_init_out(LED_0, 0);
 
     console_printf("Ostur Controller v2.0\n");
 
+    #if MYNEWT_VAL(USE_BLE)
+        beacon_data.magic = 0xDA7A;
+        beacon_data.timestamp = 0;
+    #endif
+
     while (1) {
 
-        os_time_delay(OS_TICKS_PER_SEC);
+        os_time_delay(OS_TICKS_PER_SEC * 2);
 
-        hal_gpio_write(LED_0, (count >> 0) & 0x1);
+        ble_app_advertise();
+        hal_gpio_toggle(LED_0);
 
-        count++;
+        timestamp++;
     }
 }
+
+
 
 int
 main(int argc, char **argv)
@@ -44,6 +108,10 @@ main(int argc, char **argv)
         OS_WAIT_FOREVER,
         ostur_task_stack,
         OSTUR_STACK_SIZE);
+
+#if MYNEWT_VAL(USE_BLE)
+     ble_hs_cfg.sync_cb = ble_app_on_sync;
+#endif
 
     while(1) {
         os_eventq_run(os_eventq_dflt_get());
