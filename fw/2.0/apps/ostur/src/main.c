c3d086916641049652d098d3f5eb3a339b9c171d
alvaro <gitkraken@alvaroprieto.com>
2018-02-28 05:35:33 -0800

    Adding fridge control

--- fw/2.0/apps/ostur/src/main.c
+++ fw/2.0/apps/ostur/src/main.c
@@ -19,7 +19,14 @@
 struct os_task ostur_task;
 os_stack_t ostur_task_stack[OSTUR_STACK_SIZE];
 
-static uint16_t timestamp;
+#define FRIDGE_TASK_PRI         (20)
+#define FRIDGE_STACK_SIZE       (64)
+struct os_task fridge_task;
+os_stack_t fridge_task_stack[FRIDGE_STACK_SIZE];
+
+static volatile uint16_t timestamp;
+
+static volatile bool fridge_on;
 
 #if MYNEWT_VAL(USE_BLE)
 #define BEACON_MAGIC 0xDA7A
@@ -75,7 +82,73 @@ static void ble_app_on_sync(void) {
 }
 #endif
 
+#define NUM_SAMPLES 8
+
+static int16_t primary_buff[NUM_SAMPLES];
+static int16_t outside_buff[NUM_SAMPLES];
+static uint16_t buff_index;
+
+static const int16_t temp_setting = (int16_t)(MYNEWT_VAL(TEMP_SETTING_C) * 100);
+
+#define MAX_SENSORS 8
+#define PRIMARY_SENSOR 0
+#define OUTSIDE_SENSOR (7)
+
+typedef struct {
+  uint8_t bus;
+  uint8_t addr;
+  bool enabled;
+  int16_t temperature;
+  int16_t humidity;
+} th_sensor_t;
+
+th_sensor_t sensors[MAX_SENSORS] = {
+    {0, SHT3x_ADDR,     false},
+    {0, SHT3x_ALT_ADDR, false},
+    {1, SHT3x_ADDR,     false},
+    {1, SHT3x_ALT_ADDR, false},
+    {2, SHT3x_ADDR,     false},
+    {2, SHT3x_ALT_ADDR, false},
+    {3, SHT3x_ADDR,     false},
+    {3, SHT3x_ALT_ADDR, false},
+};
+
+void fridge_control(bool enable) {
+    if(enable) {
+        hal_gpio_write(FRIDGE_PIN, 1);
+        hal_gpio_write(LED_2_PIN, 1);
+        console_printf("Enabling fridge\n");
+    } else {
+        hal_gpio_write(FRIDGE_PIN, 0);
+        hal_gpio_write(LED_2_PIN, 0);
+        console_printf("Disabling fridge\n");
+    }
+}
+
+void fridge_task_fn(void *arg) {
+    bool fridge_running = false;
+
+    hal_gpio_init_out(FRIDGE_PIN, 0);
+    hal_gpio_init_out(LED_2_PIN, 0);
+
+    while(1) {
+        if(!fridge_running && fridge_on) {
+            fridge_running = true;
+            fridge_control(true);
+        } else if(fridge_running && !fridge_on) {
+            fridge_running = false;
+            fridge_control(false);
+            os_time_delay(MYNEWT_VAL(FRIDGE_OFF_TIME_M) * OS_TICKS_PER_SEC * 60);
+        }
+
+        os_time_delay(OS_TICKS_PER_SEC);
+    }
+}
+
 void ostur_task_fn(void *arg) {
+    int32_t outside_avg;
+    int32_t primary_avg;
+
     hal_gpio_init_out(LED_0_PIN, 0);
     hal_gpio_init_out(TCA_NRST_PIN, 1);
 
@@ -88,25 +161,84 @@ void ostur_task_fn(void *arg) {
         beacon_data.timestamp = 0;
     #endif
 
+    console_printf("Detecting HT Sensors\n");
+    int32_t rval;
+    for(uint8_t sensor=0; sensor < MAX_SENSORS; sensor++) {
+        tca95xxa_set_channel(0x70, sensors[sensor].bus);
+        rval = sht3x_init(sensors[sensor].addr);
+        console_printf("%d:0x%02X - %ld\n", sensors[sensor].bus, SHT3x_ADDR, rval);
+        if (rval == 0) {
+            sensors[sensor].enabled = true;
+        }
+    }
 
-    tca95xxa_set_channel(0x70, 3);
+    if(sensors[OUTSIDE_SENSOR].enabled == false) {
+        console_printf("Error! Outside sensor not present\n");
+    }
 
-    sht3x_init(SHT3x_ALT_ADDR);
+    if(sensors[PRIMARY_SENSOR].enabled == false) {
+        console_printf("Error! Primary sensor not present\n");
+    }
+
+    // Take a bunch of samples to initialize the buffer
+    for(uint16_t sample = 0; sample < NUM_SAMPLES; sample++) {
+        int16_t temp;
+        tca95xxa_set_channel(0x70, sensors[OUTSIDE_SENSOR].bus);
+        rval = sht3x_read(sensors[OUTSIDE_SENSOR].addr, &temp, NULL);
+        outside_buff[sample] = temp;
+
+        tca95xxa_set_channel(0x70, sensors[PRIMARY_SENSOR].bus);
+        rval = sht3x_read(sensors[PRIMARY_SENSOR].addr, &temp, NULL);
+        primary_buff[sample] = temp;
+    }
+
+    console_printf("Temp setting: %d\n", temp_setting);
 
     while (1) {
-        int16_t temp;
-        int16_t humidity;
+        os_time_delay(OS_TICKS_PER_SEC * MYNEWT_VAL(SAMPLE_RATE_S));
 
-        os_time_delay(OS_TICKS_PER_SEC);
+        for(uint8_t sensor=0; sensor < MAX_SENSORS; sensor++) {
+            if (!sensors[sensor].enabled) {
+                continue;
+            }
+            int16_t *temp = &sensors[sensor].temperature;
+            int16_t *humidity = &sensors[sensor].humidity;
 
-        sht3x_read(SHT3x_ALT_ADDR, &temp, &humidity);
+            tca95xxa_set_channel(0x70, sensors[sensor].bus);
+            rval = sht3x_read(sensors[sensor].addr, temp, humidity);
+            console_printf("s: %d t:%d h:%d\n", sensor, *temp, *humidity);
+        }
 
-        beacon_data.temperature = temp;
-        beacon_data.humidity = humidity;
+        // Update primary/outside buffers for average
+        outside_buff[buff_index] = sensors[OUTSIDE_SENSOR].temperature;
+        primary_buff[buff_index] = sensors[PRIMARY_SENSOR].temperature;
+        buff_index =  (buff_index + 1) & (NUM_SAMPLES - 1);
 
-        console_printf("t:%d h:%d\n", temp, humidity);
+        outside_avg = 0;
+        primary_avg = 0;
+
+        // Compute averages
+        for(uint16_t index = 0; index < NUM_SAMPLES; index++){
+            outside_avg += outside_buff[index];
+            primary_avg += primary_buff[index];
+        }
+
+        outside_avg /= NUM_SAMPLES;
+        primary_avg /= NUM_SAMPLES;
+
+        if ((primary_avg > temp_setting) && (temp_setting < outside_avg)) {
+            fridge_on = true;
+        } else {
+            fridge_on = false;
+        }
+
+#if MYNEWT_VAL(USE_BLE)
+        beacon_data.temperature = sensors[OUTSIDE_SENSOR].temperature;
+        beacon_data.humidity = sensors[OUTSIDE_SENSOR].humidity;
 
         ble_app_advertise();
+#endif
+
         hal_gpio_toggle(LED_0_PIN);
 
         timestamp++;
@@ -114,11 +246,20 @@ void ostur_task_fn(void *arg) {
 }
 
 
-
 int
 main(int argc, char **argv)
 {
     sysinit();
+
+    os_task_init(
+        &fridge_task,
+        "fridge_task",
+        fridge_task_fn,
+        NULL,
+        FRIDGE_TASK_PRI,
+        OS_WAIT_FOREVER,
+        fridge_task_stack,
+        FRIDGE_STACK_SIZE);
 
     os_task_init(
         &ostur_task,
