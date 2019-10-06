9d79a1d448551c4537f533de3d89833b471ff0fa
Alvaro Prieto <source@alvaroprieto.com>
2019-10-06 12:50:44 -0700

    Adding ostur_usb_scanner.py and supporting scripts

---
+++ sw/2.0/scripts/ostur_packet.py
@@ -0,0 +1,34 @@
+import struct
+import collections
+
+
+class OsturPacket:
+    def __init__(self, name, packet_struct):
+        self.packet_struct = packet_struct
+        self.name = name
+        self.named_tuple = collections.namedtuple(
+            name, list(list(zip(*packet_struct))[0])
+        )
+        self.struct_str = "<" + "".join(list(list(zip(*packet_struct))[1]))
+
+    def from_dict(self, packet_dict):
+        items = []
+        for item in self.packet_struct:
+            items.append(packet_dict[item[0]])
+
+        return self.named_tuple._make(items)
+
+    def decode(self, packet):
+        return self.named_tuple._make(struct.unpack_from(self.struct_str, packet, 0))
+
+    def encode(self, args):
+        return struct.pack(self.struct_str, *args)
+
+    def round(self, namedtuple):
+        # Round all floating point items to 3 decimal places
+        # TODO - have precision and other options set in packet_struct
+        data_dict = namedtuple._asdict()
+        for key, val in data_dict.items():
+            if isinstance(val, float):
+                data_dict[key] = round(val, 3)
+        return self.named_tuple(**data_dict)
---
+++ sw/2.0/serial_packet/setup.py
@@ -0,0 +1,17 @@
+"""
+Testing
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="serial_library",
+    version="0.1",
+    description="Serial Packet Library",
+    url="https://github.com/alvarop/ostur",
+    author="Alvaro Prieto",
+    author_email="source@alvaroprieto.com",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
